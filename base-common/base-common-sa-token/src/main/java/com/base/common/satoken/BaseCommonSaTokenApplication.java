94588e40cdaaf179edbbdd5f44677f4dede4ad0e
九月 <limin04551@163.com>
2023-03-11 11:31:22 +0800

    feat:增加sa-token认证框架模块

---
+++ base-common/base-common-sa-token/src/main/java/com/base/common/satoken/BaseCommonSaTokenApplication.java
@@ -0,0 +1,13 @@
+package com.base.common.satoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseCommonSaTokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseCommonSaTokenApplication.class, args);
+    }
+
+}
---
+++ base-common/base-common-sa-token/src/test/java/com/base/common/satoken/BaseCommonSaTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.base.common.satoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseCommonSaTokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
