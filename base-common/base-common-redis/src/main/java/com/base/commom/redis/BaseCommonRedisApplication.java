cf2f48a9af541b91c87dc31d1a6e42367f9471cd
九月 <limin04551@163.com>
2023-03-12 21:07:41 +0800

    feat:新增Redis相关模块

---
+++ base-common/base-common-redis/src/main/java/com/base/commom/redis/BaseCommonRedisApplication.java
@@ -0,0 +1,13 @@
+package com.base.commom.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseCommonRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseCommonRedisApplication.class, args);
+	}
+
+}
